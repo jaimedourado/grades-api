8e1e91119f653f15b6cf8f4758d75cbbd5117d1f
jaime.dourado <jaycdourado@gmail.com>
2020-09-06 18:33:16 -0300

    Define cors para heroku app

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://jaydourado-grades-app.herokuapp.com/',
   })
 );
 
