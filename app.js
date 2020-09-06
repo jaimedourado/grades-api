cfaef463b0f3679051a9af7d2b6cb49267dacf48
jaime.dourado <jaycdourado@gmail.com>
2020-09-06 19:59:33 -0300

    Uma modificação no cors

--- app.js
+++ app.js
@@ -24,9 +24,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://jaydourado-grades-app.herokuapp.com/',
+    origin: '*',
+    optionsSuccessStatus: 200,
   })
 );
+//origin: 'https://jaydourado-grades-app.herokuapp.com/'
 
 app.use(gradeRouter);
 app.get('/', (req, res) => {
